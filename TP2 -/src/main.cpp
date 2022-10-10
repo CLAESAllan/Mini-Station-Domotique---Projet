20327b31952b20e856491468b1cbe97021695f96
GillesSlotte <79530746+GillesSlotte@users.noreply.github.com>
2022-10-10 10:33:12 +0200

    DHT non FS oui

--- TP2 -/src/main.cpp
+++ TP2 -/src/main.cpp
@@ -18,6 +18,47 @@ AsyncWebServer server(80);
 const char *ssid = "Ordi de Gilles";
 const char *password = "12345678";
 
+#define DHTPIN 26     // Digital pin connected to the DHT sensor
+#define DHTTYPE    DHT11     // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+String readDHTTemperature() {
+  float t = dht.readTemperature();
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(t);
+    return String(t);
+  }
+}
+
+String readDHTHumidity() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(h);
+    return String(h);
+  }
+}
+
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return readDHTTemperature();
+  }
+  else if(var == "HUMIDITY"){
+    return readDHTHumidity();
+  }
+  return String();
+}
+
 const char *PARAM_MESSAGE = "message";
 
 void notFound(AsyncWebServerRequest *request)
@@ -28,15 +69,8 @@ void notFound(AsyncWebServerRequest *request)
 void setup()
 {
 
-    Serial.begin(9600);
-
-    /*/ Initialize LittleFS
-    if (!LITTLEFS.begin())
-    {
-        Serial.println("An Error has occurred while mounting LittleFS");
-        return;
-    }
-    /*/
+    Serial.begin(9600); 
+    SPIFFS.begin();
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
@@ -52,9 +86,11 @@ void setup()
     // Route for root / web page
 
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        SPIFFS.begin();
-        File f = SPIFFS.open("/index.html","r");
-        request->send(200, "text/plain", f);
+        request->send(SPIFFS, "/index.html", String(), false, processor);
+    });
+
+    server.on("/index.css", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(SPIFFS, "/index.css","text/css");
     });
 
     // Send a GET request to <IP>/get?message=<message>
@@ -78,6 +114,13 @@ void setup()
             message = "No message sent";
         }
         request->send(200, "text/plain", "Hello, POST: " + message); });
+
+    server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+  request->send_P(200, "text/plain", readDHTTemperature().c_str());
+});
+server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+  request->send_P(200, "text/plain", readDHTHumidity().c_str());
+});
 
     server.onNotFound(notFound);
 
