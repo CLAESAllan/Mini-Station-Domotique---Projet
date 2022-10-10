60d8273db4d87fcd027f69349bb794d4cace5dd8
GillesSlotte <79530746+GillesSlotte@users.noreply.github.com>
2022-10-10 09:03:01 +0200

    Update main.cpp

--- TP2 -/src/main.cpp
+++ TP2 -/src/main.cpp
@@ -10,31 +10,38 @@
 #include <DHT.h>
 #include <Adafruit_SSD1306.h>
 #include <DHT_U.h>
+#include <SPIFFS.h>
+#include <FS.h>
 
 AsyncWebServer server(80);
 
-const char* ssid = "Ordi de Gilles";
-const char* password = "12345678";
+const char *ssid = "Ordi de Gilles";
+const char *password = "12345678";
 
-const char* PARAM_MESSAGE = "message";
+const char *PARAM_MESSAGE = "message";
 
-void notFound(AsyncWebServerRequest *request) {
+void notFound(AsyncWebServerRequest *request)
+{
     request->send(404, "text/plain", "Not found");
 }
 
-void setup() {
+void setup()
+{
 
     Serial.begin(9600);
 
-     // Initialize LittleFS
-  if(!LITTLEFS.begin()){
-    Serial.println("An Error has occurred while mounting LittleFS");
-    return;
-  }
+    /*/ Initialize LittleFS
+    if (!LITTLEFS.begin())
+    {
+        Serial.println("An Error has occurred while mounting LittleFS");
+        return;
+    }
+    /*/
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
         Serial.printf("WiFi Failed!\n");
         return;
     }
@@ -43,40 +50,47 @@ void setup() {
     Serial.println(WiFi.localIP());
 
     // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LITTLEFS, "/index.html", String(), false);
-  });
 
-    /*:server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(200, "text/plain", "Gilles Slotte");
-    });/*/
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        SPIFFS.begin();
+        File f = SPIFFS.open("/index.html","r");
+        request->send(200, "text/plain", f);
+    });
 
     // Send a GET request to <IP>/get?message=<message>
-    server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
         String message;
         if (request->hasParam(PARAM_MESSAGE)) {
             message = request->getParam(PARAM_MESSAGE)->value();
         } else {
             message = "No message sent";
         }
-        request->send(200, "text/plain", "Hello, GET: " + message);
-    });
+        request->send(200, "text/plain", "Hello, GET: " + message); });
 
     // Send a POST request to <IP>/post with a form field message set to <message>
-    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
+    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request)
+              {
         String message;
         if (request->hasParam(PARAM_MESSAGE, true)) {
             message = request->getParam(PARAM_MESSAGE, true)->value();
         } else {
             message = "No message sent";
         }
-        request->send(200, "text/plain", "Hello, POST: " + message);
-    });
+        request->send(200, "text/plain", "Hello, POST: " + message); });
 
     server.onNotFound(notFound);
 
     server.begin();
 }
 
-void loop() {
+void loop()
+{/*/
+    SPIFFS.begin();
+    File f = SPIFFS.open("/index.html","r");
+        if (!f){
+            Serial.print("Fichier ouverture raté");
+        }
+    f.close();
+    /*/
 }
