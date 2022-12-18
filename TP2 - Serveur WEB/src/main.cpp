97874d8236f4706766d2fb539a84488dd37dca64
GillesSlotte <79530746+GillesSlotte@users.noreply.github.com>
2022-12-18 17:50:33 +0100

    TP 2 - Commenté

--- TP2 - Serveur WEB/src/main.cpp
+++ TP2 - Serveur WEB/src/main.cpp
@@ -1,3 +1,4 @@
+//Libraries
 #include <Arduino.h>
 #ifdef ESP32
 #include <WiFi.h>
@@ -13,16 +14,21 @@
 #include <SPIFFS.h>
 #include <FS.h>
 
+//Create a Web Server object to listen for incoming HTTP requests on port 80.
 AsyncWebServer server(80);
 
+//Allows you to connect to the wifi point
 const char *ssid = "Ordi de Gilles";
 const char *password = "12345678";
-const char* PARAM_INPUT_1 = "state";
 
+const char* PARAM_INPUT_1 = "state"; 
+
+//Pin for DHT11 + type of DHT
 #define DHTPIN 26     // Digital pin connected to the DHT sensor
 #define DHTTYPE    DHT11     // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
 
+//Pin used
 #define BP 25
 #define LED 27
 
@@ -35,6 +41,7 @@ int lastButtonState = LOW;   // the previous reading from the input pin
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
+//Read the temperature of DHT11
 String readDHTTemperature() {
   float t = dht.readTemperature();
   if (isnan(t)) {    
@@ -46,6 +53,7 @@ String readDHTTemperature() {
     return String(t);
   }
 }
+//Read the state of the LED
 String outputState(){
   if(digitalRead(LED)){
     return "checked";
@@ -57,41 +65,34 @@ String outputState(){
 }
 
 String processor(const String& var){
-  //Serial.println(var);
   if(var == "TEMPERATURE"){
     return readDHTTemperature();
   }
-  
- /*/ if(var == "BUTTONPLACEHOLDER"){
-    String buttons ="";
-    String outputStateValue = outputState();
-    buttons+= "<p4>LED<span id=\"outputState\"></span></p4><label class=\"switch\"><input type=\"button\" onclick=\"toggleCheckbox(this)\" id=\"output\" " + outputStateValue ;//+ ">//<span class=\"slider\"></span></label>"
-    return buttons;
-  }/*/
+
   return String();
 }
 
 
 const char *PARAM_MESSAGE = "message";
 
-void notFound(AsyncWebServerRequest *request)
-{
+//If no connection between the ESP32 and the web server then returns a 404 error. 
+void notFound(AsyncWebServerRequest *request){
     request->send(404, "text/plain", "Not found");
 }
 
-void setup()
-{
+void setup(){
+    Serial.begin(115200); //Setup the monitor serial
 
-    Serial.begin(115200); 
+    //Defines I/O
     pinMode(LED, OUTPUT);
     digitalWrite(LED, LOW);
     pinMode(BP, INPUT);
-    SPIFFS.begin();
 
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED)
-    {
+    SPIFFS.begin(); //Initialize SPIFFS
+
+    WiFi.mode(WIFI_STA); //ESP32 connects to an access point
+    WiFi.begin(ssid, password); //Initialize the network settingd of the and provides the current status.
+    if (WiFi.waitForConnectResult() != WL_CONNECTED){
         Serial.printf("WiFi Failed!\n");
         return;
     }
@@ -99,8 +100,9 @@ void setup()
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
 
+    /*Server.on defines a callback function. 
+    *This registers a function that will be executed when a certain person moves to a later date.*/
     // Route for root / web page
-
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
         request->send(SPIFFS, "/index.html", String(), false, processor);
     });
@@ -108,93 +110,93 @@ void setup()
     server.on("/index.css", HTTP_GET, [](AsyncWebServerRequest *request){
         request->send(SPIFFS, "/index.css","text/css");
     });
-
+    
+    //Sends temperature data to the WebServer
     server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-  request->send_P(200, "text/plain", readDHTTemperature().c_str());
-  });
+        request->send_P(200, "text/plain", readDHTTemperature().c_str());
+    });
 
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
-  Serial.println("flag update");   
-  String inputMessage;
-  String inputParam;
-  // GET input1 value on <ESP_IP>/update?state=<inputMessage>
-  if (true/*/request->hasParam(PARAM_INPUT_1)/*/) {
-    inputMessage = request->getParam(PARAM_INPUT_1)->value();
-    inputParam = PARAM_INPUT_1;
-    digitalWrite(LED, inputMessage.toInt());
-    ledState = !ledState;
-  }
-  else {
-    inputMessage = "No message sent";
-    inputParam = "none";
-  }
-  Serial.println(inputMessage);
-  request->send(200, "text/plain", "OK");
-});
-
-server.on("/state", HTTP_GET, [] (AsyncWebServerRequest *request) {
-  request->send(200, "text/plain", String(digitalRead(LED)).c_str());
-});
-
-
-    // Send a GET request to <IP>/get?message=<message>
-    server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
-              {
-        String message;
-        if (request->hasParam(PARAM_MESSAGE)) {
-            message = request->getParam(PARAM_MESSAGE)->value();
-        } else {
-            message = "No message sent";
+    //Update of the LED status on the Webserver
+    server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+        Serial.println("flag update");   
+        String inputMessage;
+        String inputParam;
+        // GET input1 value on <ESP_IP>/update?state=<inputMessage>
+        if (true/*/request->hasParam(PARAM_INPUT_1)/*/) {
+          inputMessage = request->getParam(PARAM_INPUT_1)->value();
+          inputParam = PARAM_INPUT_1;
+          digitalWrite(LED, inputMessage.toInt());
+          ledState = !ledState;
         }
-        request->send(200, "text/plain", "Hello, GET: " + message); });
-
-    // Send a POST request to <IP>/post with a form field message set to <message>
-    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request)
-              {
-        String message;
-        if (request->hasParam(PARAM_MESSAGE, true)) {
-            message = request->getParam(PARAM_MESSAGE, true)->value();
-        } else {
-            message = "No message sent";
+        else {
+          inputMessage = "No message sent";
+          inputParam = "none";
         }
-        request->send(200, "text/plain", "Hello, POST: " + message); });
+        Serial.println(inputMessage);
+        request->send(200, "text/plain", "OK");
+    });
 
-    server.onNotFound(notFound);
+    //Update of the button status on the Webserver
+    server.on("/state", HTTP_GET, [] (AsyncWebServerRequest *request) {
+      request->send(200, "text/plain", String(digitalRead(LED)).c_str());
+    });
 
-    server.begin();
+    //Send a GET request to <IP>/get?message=<message>
+    server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request){
+      String message;
+      if (request->hasParam(PARAM_MESSAGE)) {
+        message = request->getParam(PARAM_MESSAGE)->value();
+      } 
+      else {
+        message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, GET: " + message); });
+
+    //Send a POST request to <IP>/post with a form field message set to <message>
+    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
+      String message;
+      if (request->hasParam(PARAM_MESSAGE, true)) {
+        message = request->getParam(PARAM_MESSAGE, true)->value();
+      } 
+      else {
+      message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, POST: " + message); });
+
+    server.onNotFound(notFound); //When there is no request from our client, it will send a 404 error message saying nothing found.
+    server.begin(); // Start server
 }
 
-void loop()
-{
-// read the state of the switch into a local variable:
+void loop(){
+  //Read the state of the switch into a local variable:
   int reading = digitalRead(BP);
-  // check to see if you just pressed the button
-  // (i.e. the input went from LOW to HIGH), and you've waited long enough
-  // since the last press to ignore any noise:
+  /*Check to see if you just pressed the button
+  *(i.e. the input went from LOW to HIGH), and you've waited long enough
+  *since the last press to ignore any noise: */
 
-  // If the switch changed, due to noise or pressing:
+  //If the switch changed, due to noise or pressing:
   if (reading != lastButtonState) {
-    // reset the debouncing timer
+    //Reset the debouncing timer
     lastDebounceTime = millis();
   }
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
+    /*Whatever the reading is at, it's been there for longer than the debounce
+    *delay, so take it as the actual current state:*/
 
-    // if the button state has changed:
+    //If the button state has changed:
     if (reading != buttonState) {
       buttonState = reading;
 
-      // only toggle the LED if the new button state is HIGH
+      //Only toggle the LED if the new button state is HIGH
       if (buttonState == HIGH) {
         ledState = !ledState;
       }
     }
   }
-  // set the LED:
+  //Set the LED:
   digitalWrite(LED, ledState);
 
-  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  //Save the reading. Next time through the loop, it'll be the lastButtonState:
   lastButtonState = reading;
 }
