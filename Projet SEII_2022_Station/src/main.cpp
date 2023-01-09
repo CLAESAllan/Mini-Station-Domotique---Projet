477a92a253f2e713507bf55a5e3786635eb17c25
GillesSlotte <79530746+GillesSlotte@users.noreply.github.com>
2023-01-09 12:15:00 +0100

    updt

--- Projet SEII_2022_Station/src/main.cpp
+++ Projet SEII_2022_Station/src/main.cpp
@@ -1,4 +1,4 @@
-//Librairies
+//  Librairies
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <WiFi.h>
@@ -7,25 +7,25 @@
 #include <DHT_U.h>
 #include <ESP32Servo.h>
 
-//Définition des variables
+//  Définition des variables
 #define DHTPin 4
-#define DHTTYPE DHT22 //Type de capteur
+#define DHTTYPE DHT22 //  Type de capteur
 #define BP 12
 #define LED 32
 
-//Définition des variables en int
+//  Définition des variables en int
 int pinREED = 19;
 int lastCheck = 0;
 int lastEtatPorte = 0;
 int porteForce;
 
-//Définition des variables en float
+//  Définition des variables en float
 float lastTemp;
 float lastHum;
 float newTemp;
 float newHum;
 
-//Définition des variables en string
+//  Définition des variables en string
 String CodeRecu;
 String etatReset;
 
@@ -39,30 +39,30 @@ char json_DHT22[256];
 char json_Reed[256];
 char json_EtatPorte[256];
 
-//Permet de se connecter au point d'accès
-const char* ssid = "Ordi de Gilles";  // SSID du réseau Wi-Fi auquel nous devons nous connecter
-const char* password = "12345678";  //Mot de passe du réseau Wi-Fi
+//  Permet de se connecter au point d'accès
+const char* ssid = "Ordi de Gilles";  //  SSID du réseau Wi-Fi auquel nous devons nous connecter
+const char* password = "12345678";  //  Mot de passe du réseau Wi-Fi
 
-//Connection au serveur MQTT
-const char* mqtt_server = "192.168.137.1";  //Adresse IP du server MQTT
+//  Connection au serveur MQTT
+const char* mqtt_server = "192.168.137.1";  //  Adresse IP du server MQTT
 
-//Initialisations de l'espClient.
-WiFiClient espClient; //Création de "espClient" propre au client Wi-Fi
-PubSubClient client(espClient);  //Création d'un système de messagerie MQTT du client
+//  Initialisations de l'espClient.
+WiFiClient espClient; //  Création de "espClient" propre au client Wi-Fi
+PubSubClient client(espClient);  // Création d'un système de messagerie MQTT du client
 
-Servo myservo;  //Création d'un objet servo pour contrôler le servo
+Servo myservo;  //  Création d'un objet servo pour contrôler le servo
 
-//Initialisation du capteur DHT.
+//  Initialisation du capteur DHT.
 DHT dht(DHTPin, DHTTYPE);
-//Variables auxiliaires des minuteurs
+//  Variables auxiliaires des minuteurs
 long now = millis();
 long lastMeasure = 0;
 String clientId = "Hello";
 
-//Cette fonction permet de connecter votre ESP32 à votre routeur.
+//  Cette fonction permet de connecter votre ESP32 à votre routeur.
 void setup_wifi() {
   delay(10);  //Délai de 10ms
-  //Nous commençons par nous connecter à notre réseau WiFi
+  //  Nous commençons par nous connecter à notre réseau WiFi
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -82,25 +82,25 @@ void setup_wifi() {
  *de sorte que lorsque le dispositif envoie un message à un sujet auquel votre ESP32 est abonné.
 */
 
-//Fonction de Callback quand un message MQTT arrive sur un topic (subscribe).
+//  Fonction de Callback quand un message MQTT arrive sur un topic (subscribe).
 void callback(String topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);  // On affiche de quel topic il s'agit
   Serial.print(". Message: ");
   String messageTemp;
 
-  for (int i = 0; i < length; i++) {  //Nousn relions tous les caractères reçus sur le topic.
+  for (int i = 0; i < length; i++) {  //  Nousn relions tous les caractères reçus sur le topic.
     Serial.print((char)message[i]);
     messageTemp += (char)message[i];  //Nous enregistrons tous les caractères que nous avons besoin (uniquement).
   }
   Serial.println();
-  if(topic == "JSON_EtatCode"){ //Nous vérifions si c'est le bon topic.
+  if(topic == "JSON_EtatCode"){ //  Nous vérifions si c'est le bon topic.
     CodeRecu = messageTemp;
   }
-  if(topic == "JSON_resetInfractionOUT"){ //Nous vérifions si c'est le bon topic.
+  if(topic == "JSON_resetInfractionOUT"){ //  Nous vérifions si c'est le bon topic.
     etatReset = messageTemp;
   }
-  if (topic == "BpManuelPorte"){  //Nous vérifions si c'est le bon topic.
+  if (topic == "BpManuelPorte"){  //  Nous vérifions si c'est le bon topic.
     porteForce = messageTemp.toInt();
     Serial.println(porteForce);
   }
@@ -112,7 +112,7 @@ void reconnect() {
   // Boucle jusqu'à ce que nous soyons reconnectés
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    //Tentative de connexion
+    //  Tentative de connexion
  
     if (client.connect(clientId.c_str(),"","095f3cdd2282")) {
       Serial.println("connected");
@@ -121,10 +121,10 @@ void reconnect() {
       client.subscribe("JSON_EtatCode");
       client.subscribe("BpManuelPorte");
     } else {  // Si la connexion rate
-      Serial.print("failed, rc=");  //Nous affichons qu'il y a une erreur.
-      Serial.print(client.state());  //Nous affichons le numéro de l'erreur.
-      Serial.println(" try again in 5 seconds");   //Nous affichons que nous réessayons.
-      //Nous attendons 5 secondes avant de réessayer.
+      Serial.print("failed, rc=");  //  Nous affichons qu'il y a une erreur.
+      Serial.print(client.state());  // Nous affichons le numéro de l'erreur.
+      Serial.println(" try again in 5 seconds");   // Nous affichons que nous réessayons.
+      //  Nous attendons 5 secondes avant de réessayer.
       delay(5000);  
     }
   }
@@ -140,22 +140,22 @@ int checkDHT22(){
 }
 
 void setup() {
-  // Initialize pins used
+  //  Initialisation des broches utilisées
   pinMode(LED, OUTPUT);
   pinMode(BP, INPUT);
 
-  myservo.attach(13);  // attaches the servo on pin 13 to the servo object
+  myservo.attach(13);  // Nous attachons le servo sur la broche 13.
  
-  dht.begin();//  module DHT11
+  dht.begin();  //  Module DHT22
 
-  Serial.begin(115200); //Initialize serial monitor
+  Serial.begin(115200); //Initialisation du moniteur série.
   setup_wifi();
-  // set server mqtt on port 1883 to the client
+  // Définis le serveur mqtt sur le port 1883 pour le client.
   client.setServer(mqtt_server, 1883);
-  // set the function callback to the client
+  // Définis la fonction callback pour le client.
   client.setCallback(callback);
-  lastHum = dht.readHumidity(); // Read humidity in %
-  lastTemp = dht.readTemperature(); //Read temperature as Celsius
+  lastHum = dht.readHumidity(); // Lis l'humidité en %.
+  lastTemp = dht.readTemperature(); //Lis la température en Celsius.
 }
 
 void loop() {
@@ -218,7 +218,7 @@ void loop() {
   return;
   }
 
-  /*/if (checkDHT22()){/*/
+  if (checkDHT22()){
 
     static char temperatureTemp[7];
     dtostrf(t, 6, 2, temperatureTemp); //La fonction dtostrf convertit les valeurs doubles et flottantes en une chaîne de caractères.
@@ -233,6 +233,6 @@ void loop() {
     serializeJson(docDHT22, json_DHT22);
     client.publish("JSON_DHT22",json_DHT22);
     Serial.println(json_DHT22);
-    /*/}/*/
+    }
   }
 }
